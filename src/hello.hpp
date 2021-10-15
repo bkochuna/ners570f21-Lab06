38ebec8a09636027ae5a94b76eabbb93c808ab07
Brendan Kochunas <bkochuna@umich.edu>
2021-10-15 13:30:11 -0400

    This commit contains all the work from lab 07

--- src/hello.hpp
+++ src/hello.hpp
@@ -1,6 +1,31 @@
+#ifndef _SPARSE_MATRIX_
+#define _SPARSE_MATRIX_
+
 #include <iostream>
+#include <cstddef> //for size_t
+#include <map>     //for using map (like a Python dictionary)
+#include <utility> //for using pair
 
 namespace spmv
 {
-  void hello();
+  template <class fp_type>
+  class SparseMatrix
+  {
+    //private:
+
+  protected:
+    size_t _nrows = 0;
+    size_t _ncols = 0;
+    size_t _nz = 0;
+
+    std::map<std::pair<size_t, size_t>, fp_type> _buildCoeff;
+
+  public:
+    //This is the constructor
+    SparseMatrix(const size_t nrows, const size_t ncols);
+    ~SparseMatrix(); //This is the destructor
+  };
+
 }
+
+#endif
--- test/testHello.cpp
+++ test/testHello.cpp
@@ -3,6 +3,16 @@
 using namespace spmv;
 
 int main() {
-  hello();
+  int i;
+
+  //Define a scoping unit
+  std::cout << "Before  scoping unit" << std::endl;
+  {
+    SparseMatrix<double> A_double(10,10); //Call the constructor
+    SparseMatrix<float> A_float(10,10); 
+    std::cout << "After variable definition" << std::endl;
+  } // Destructor will be called here
+  std::cout << "After scoping unit" << std::endl;
+
   return 0;
 }
