d11e2311517076ec883c4361847baeeff74c0e58
Emily Marie Oliphant <olipemil@gl-login1.arc-ts.umich.edu>
2021-10-27 18:07:10 -0400

    Changes to the getCRS function to define the global variables nz, ncols, and nrows and a matrix Aji from the CRS vector vals, ia, and ja
    Edits where completed by Kseniia and Emily

--- src/CRS.cpp
+++ src/CRS.cpp
@@ -12,6 +12,22 @@ void CRS<fp_type>::setCoefficients(const std::map< std::pair<size_t,size_t> , fp
 
 template <class fp_type>
 void CRS<fp_type>::getCRS(std::vector<fp_type> &vals, std::vector<size_t> &ia, std::vector<size_t> &ja)  const {
+    
+    int nz = 0
+    &ncols = *(&ia + 1) - ia - 1;
+    &nrows = *(&ia + 1) - ia - 1;
+    for (i = 1; i<=&ncols; i=i+1) {//row
+        for (j = 1; j<=&ncols; j=j+1) { // coloumn
+            if ((&ja[nz] == j) && (nz <= &ia[i])) {
+                A[i][j] = &vals[&nz];
+                nz = nz+1;
+            }
+            else {
+                A[i][j] = 0;
+            }
+        }
+    }
+    &nz = nz
 
 }
 
